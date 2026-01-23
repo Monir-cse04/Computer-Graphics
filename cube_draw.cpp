0b277f0672c4e643da2c38cde99cb03767f0066f
Monir Ahmed <152847216+Monir-cse04@users.noreply.github.com>
2026-01-23 22:55:27 +0600

    Add files via upload
    this cube draw by applying c++ code

---
+++ cube_draw.cpp
@@ -0,0 +1,21 @@
+
+#include <bits/stdc++.h>
+#include<graphics.h>
+using namespace std;
+
+int main()
+{
+    int gd,gm;
+    detectgraph(&gd,&gm);
+    initgraph(&gd,&gm,"BGI");
+    setcolor(GREEN);
+    rectangle(50,50,200,150);
+    rectangle(100,100,250,200);
+    line(50,150,100,200);
+    line(50,50,100,100);
+    line(200,150,250,200);
+    line(200,50,250,100);
+    outtextxy(170,230,"CUBE");
+    getchar();
+    return 0;
+}
