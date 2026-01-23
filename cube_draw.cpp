f71665e69fac323a07c504b2afcc646acd6fc8e5
Monir Ahmed <152847216+Monir-cse04@users.noreply.github.com>
2026-01-24 01:30:36 +0600

    cube_draw.cpp

--- cube_draw.cpp
+++ cube_draw.cpp
@@ -8,14 +8,20 @@ int main()
     int gd,gm;
     detectgraph(&gd,&gm);
     initgraph(&gd,&gm,"BGI");
+    
     setcolor(GREEN);
+    
     rectangle(50,50,200,150);
     rectangle(100,100,250,200);
+    
     line(50,150,100,200);
     line(50,50,100,100);
     line(200,150,250,200);
     line(200,50,250,100);
-    outtextxy(170,230,"CUBE");
+    
+    outtextxy(170,230,"CUBE"); //assign text
+    
     getchar();
     return 0;
 }
+
