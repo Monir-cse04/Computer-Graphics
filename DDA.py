faf4d850e220b72385b02326f13918293697b030
Monir Ahmed <152847216+Monir-cse04@users.noreply.github.com>
2026-04-16 01:07:19 +0600

    DDA.py

--- DDA.py
+++ DDA.py
@@ -1,36 +1,31 @@
 # DDA line drawing algorithm
 from matplotlib import pyplot as plt
-
 def DDA(x0, y0, x1, y1):
-
     # find absolute differences
     dx = abs(x0 - x1)
     dy = abs(y0 - y1)
-    count = max(dx, dy) #jeta  boro oita steps
+    steps = max(dx, dy) #jeta  boro oita steps
 
     # calculate the increment in x and y
-    xinc = dx/count
-    yinc = dy/count
-
+    xinc = dx/steps
+    yinc = dy/steps
     # start with 1st point
-    xp = float(x0)
-    yp= float(y0)
-
+    x = x0
+    y= y0
     # make a list for coordinates
     x_new = []
     y_new = []
-
-    for i in range(count+1):
-        x_new.append(xp)
-        y_new.append(yp)
+    for i in range(steps):
+        x_new.append(x)
+        y_new.append(y)
         # increment the values
-        xp = xp + xinc
-        yp = yp + yinc
+        x = x + xinc
+        y = y + yinc
 
     # plot the line with coordinates list
     plt.plot(x_new, y_new)
+    plt.title("dda")
     plt.show()
-
 DDA(20,40,60,50)
 
 
